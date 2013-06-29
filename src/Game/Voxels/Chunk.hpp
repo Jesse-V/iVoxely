e0e526f4ede161c0020764b9872098e913157828
Jesse_V <jvictors@jessevictors.com>
2013-06-28 22:51:57 -0800

    Cube class

---
+++ src/Game/Voxels/Chunk.hpp
@@ -0,0 +1,10 @@
+
+#ifndef CHUNK
+#define CHUNK
+
+class Chunk
+{
+    //this class may not be necessary. I'm putting it here in case we need it
+};
+
+#endif
---
+++ src/Game/Voxels/Cube.hpp
@@ -0,0 +1,15 @@
+
+#ifndef CUBE
+#define CUBE
+
+class Cube
+{
+    //has list of 6 neighbors. If all neighbors are there, then not visible
+    //all cubes have exactly the same data, so there's no need to reassemble snippets
+    //all Cubes have access to all the lights
+    //Cube, like all Voxels (should that be a class?) should have a bool update()
+
+    //should a Cube be a Model, or should it have a Model?
+};
+
+#endif
